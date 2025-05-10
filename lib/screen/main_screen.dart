9a251c9a5f77c1de0992efa19e5700d700473cdb
sunfishes <cowjung1024@naver.com>
2025-05-10 23:02:34 +0900

    Add: DrawerHeader

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -23,10 +23,10 @@ class _MainScreenState extends State<MainScreen> {
           }, child: Text('클릭하여 서브화면으로 이동'),),
       ],
       ),
-      drawer: Drawer(child: ListView(children: [
+      drawer: Drawer(child: ListView(children: [//메인스크린에서 드로어 생성
         DrawerHeader(child: Text('헤더영역'),
         ),
-        ListTile(title: Text('홈화면'), onTap: (){
+        ListTile(title: Text('홈화면'), onTap: (){//온프레스드와 같이 홈화면을 누르면, 온탭부분에 화면이동을 추가할수있다.
           
         },),
         ListTile(title: Text('메인화면'), onTap: (){
